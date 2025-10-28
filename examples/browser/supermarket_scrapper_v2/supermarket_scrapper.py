1f4381ebef34e9c91221ea0041e312f6adf1acc1
Ana Hernández Guardiet <a.hernandez@crowdfarming.com>
2025-10-28 10:18:17 +0100

    updated .yml

--- examples/browser/supermarket_scrapper_v2/supermarket_scrapper.py
+++ examples/browser/supermarket_scrapper_v2/supermarket_scrapper.py
@@ -219,6 +219,13 @@ async def process_supermarket_task(country, supermarket, product, subtypes, llm,
                     # Headless is better for long, unsupervised runs
                     headless=  True,
                     user_data_dir=None,
+                    args = [
+                    "--no-sandbox",
+                    "--disable-setuid-sandbox",
+                    "--disable-dev-shm-usage",
+                    "--disable-gpu",
+                    "--single-process",
+                    ],
                     # minimum_wait_page_load_time=2,  # Increase wait time (if supported)
                     # wait_between_actions=0.5          # Increase action wait (if supported)
                 )
